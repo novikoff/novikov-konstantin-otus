fc3c73248adc93272bf7820afccb002bc1705cf5
kvnovikoff <kv.novikoff@gmail.com>
2020-05-17 02:36:35 +0300

    1

---
+++ vue-2/src/components/Timer.vue
@@ -0,0 +1,15 @@
+<template>
+
+        <b-form-input  placeholder="1:00" v-model="timeString" ></b-form-input>
+
+</template>
+<script>
+    export default{
+        props:['timeString'],
+        date(){
+                return{
+
+                }
+        }
+    }
+</script>
---
+++ vue-2/src/views/FirstScreen.vue
@@ -0,0 +1,13 @@
+<template>
+    <Settings/>
+</template>
+
+<script>
+    import Settings from '@/components/Settings';
+
+export default {
+    components: {
+        Settings
+    }
+}
+</script>
